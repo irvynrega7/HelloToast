cf574df2b28e5f1a9f9f6e80af1670010833d613
MrBlue <irvynrega7@gmail.com>
2019-01-24 20:07:10 +0700

    Initial Commit add code hasil count dan toast

--- app/src/main/java/com/example/oracle/hellotoast/Task1.java
+++ app/src/main/java/com/example/oracle/hellotoast/Task1.java
@@ -6,18 +6,24 @@ import android.view.View;
 import android.widget.Toast;
 
 public class Task1 extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task1);
+        mShowCount = (TextView) findviewById(R.id.show_count);
     }
 
     public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 
     public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message,Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
 
         toast.show();
     }
